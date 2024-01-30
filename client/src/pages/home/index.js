0cf414a4f2938296570dce04ebb3a812d22702fc
Pagrune <pagrune@orange.fr>
2024-01-30 22:57:54 +0100

    open chat

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -63,7 +63,7 @@ const Home = ({ token,username, setUsername, rubrique, setRubrique, titleConv, s
 
       const openChat = () => {
         document.querySelector('.le-chat').classList.toggle('open');
-        window.parent.postMessage("toggleClasses", "*");
+        // window.parent.postMessage("toggleClasses", "*");
       };
 
   return (
