5df1903f920bb8a22e53757aa10556f9e0683d55
Pagrune <pagrune@orange.fr>
2024-01-30 17:04:14 +0100

    essai 3 Lorenzo

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -68,8 +68,8 @@ const Home = ({ token,username, setUsername, rubrique, setRubrique, titleConv, s
 
   return (
     <div className='le-fond'>
-      <div className='icone open' onClick={openChat}><img src={icon} alt="Chat icon" /></div>
-      <div className={`le-chat ${token ? 'open' : ''}`}>
+      <div className='icone' onClick={openChat}><img src={icon} alt="Chat icon" /></div>
+      <div className={`le-chat open ${token ? 'open' : ''}`}>
         {token ? (
           <>
             <h1>Bienvenue sur le chat </h1>
